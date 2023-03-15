13474876ded4b552181c656bbd69a0413637f7fb
Richfield Quist <74082315+quisra@users.noreply.github.com>
2023-03-15 02:44:48 -0400

    Added a few tweaks like prompts to the user as well as a loop to keep going till user is done

--- src/IterativeSocketServer.java
+++ src/IterativeSocketServer.java
@@ -1,4 +1,5 @@
 import java.net.*;
+import java.util.*;
 import java.io.*;
 
 public class IterativeSocketServer {
@@ -9,16 +10,32 @@ public class IterativeSocketServer {
          * HOW TO CREATE A SOCKET SERVER
          * *************************************
          */
-        ServerSocket serverSocket = new ServerSocket(22);// Create a server socket
-        Socket socket = serverSocket.accept();// listen for connection
-        InputStream input = socket.getInputStream();// read data from client
-        BufferedReader reader = new BufferedReader(new InputStreamReader(input));
-        String line = reader.readLine(); // reads a line of text
-        OutputStream output = socket.getOutputStream();// Send data to client
-        PrintWriter writer = new PrintWriter(output, true);
-        writer.println("This is a message");
-        socket.close();// close the client connection
-        serverSocket.close();// terminate server
+        System.out.println("************************");
+        System.out.println("WELCOME TO THE SERVER");
+        System.out.println("************************");
+        System.out.println("What port would you like to use?");
+        Scanner scnr = new Scanner(System.in);
+        int portNum = scnr.nextInt();
+        try {
+            ServerSocket serverSocket = new ServerSocket(portNum);// Create a server socket
+            System.out.println("Directed to port " + portNum + " .....");
+            boolean notDone = true;
+            while (notDone) {
+                Socket socket = serverSocket.accept();// listen for connection
+                InputStream input = socket.getInputStream();// read data from client
+                BufferedReader reader = new BufferedReader(new InputStreamReader(input));
+                String line = reader.readLine(); // reads a line of text
+                OutputStream output = socket.getOutputStream();// Send data to client
+                PrintWriter writer = new PrintWriter(output, true);
+                writer.println("This is a message");
+                socket.close();// close the client connection
+                serverSocket.close();// terminate server
+            }
+        } catch (Exception e) {
+            // TODO: handle exception
+            System.out.println("Server exception: " + e.getMessage());
+            e.printStackTrace();
+        }
 
     }
 }
